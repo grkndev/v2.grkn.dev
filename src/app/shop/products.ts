27092492ad8fc6aba72de0e8011f7968ba440e0f
GweepCreative <gweepcreative@gmail.com>
2024-08-05 13:40:27 +0300

    shopping

---
+++ src/app/shop/products.ts
@@ -0,0 +1,39 @@
+export default [
+  {
+    title: "I can make a custom QR Menu site for your business",
+    description: `I can make QR Menu for you and your businesses with new generation technologies. Thanks to the management panel, you can easily edit, delete and add, and offer a better experience to your customers.`,
+    tags: ["QR Menu", "Cafe", "Dashboard"],
+    image: "/shop/qrmenu.jpg",
+    href: "https://bionluk.com/gweepcreative/isletmenize-ozel-qr-menu-sitesi-hazirlayabilirim-678907",
+    demoPage: "https://demoqr.grkn.dev/",
+  },
+  {
+    title: "I can make a professional website for you",
+    description:
+      "Achieve fast and usable websites using next-generation technologies",
+    tags: ["Web Development", "NextJS", "React", "TailwindCSS"],
+    image: "/shop/website.png",
+    href: "https://bionluk.com/gweepcreative/sizler-veya-kuruluslariniz-icin-website-yapabilirim-604494",
+  },
+  {
+    title: "I can make Minecraft Launcher for you",
+    description: `Hello, I can make a custom Minecraft Launcher for you or your server. A stylish and useful Minecraft Launcher for your server so that you and your users can access your server more easily and reliably...`,
+    tags: ["Minecraft", "Microsoft Login", "Cape", "Launcher"],
+    image: "/shop/minecraft.png",
+    href: "https://bionluk.com/gweepcreative/sizlerin-icin-minecraft-launcher-yapabilirim-562557",
+  },
+  {
+    title: "I can make a desktop application with c# for you",
+    description: `I can make a desktop application with C# according to your request. Thanks to these applications, you can manage and control your systems more easily.`,
+    tags: ["Desktop Development", "C#", ".NET", "WinApps"],
+    image: "/shop/csharp.jpg",
+    href: "https://bionluk.com/gweepcreative/sizler-icin-c-ile-masaustu-uygulamasi-yapabilirim-573843",
+  },
+  {
+    title: "I can make a Discord bot for your servers",
+    description: `I can make Discord bots for you and your Discord servers with the latest version, which will help you and your Discord servers to keep the server organized.`,
+    tags: ["Discord", "Bot", "Quickly"],
+    image: "/shop/discordbot.png",
+    href: "https://bionluk.com/gweepcreative/sunuculariniz-icin-discord-botu-yapabilirim-574113",
+  },
+];
