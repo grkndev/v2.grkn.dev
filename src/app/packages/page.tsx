54d4abc29ab38ccf129454a476173f6147da3b96
GweepCreative <gweepcreative@gmail.com>
2024-09-15 16:23:48 +0300

    mdx format

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -1,3 +1,4 @@
+import NpmIcon from "@/components/Icons/npm";
 import BlurFade from "@/components/magicui/blur-fade";
 import { getPackagePosts } from "@/data/blog";
 import Link from "next/link";
@@ -33,15 +34,28 @@ export default async function PackagesPage() {
           .map((post, id) => (
             <BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05} key={post.slug}>
               <Link
-                className="flex flex-col space-y-1 mb-4 backdrop-blur-sm bg-zinc-900 rounded-xl p-2"
+                className="flex flex-col space-y-1 mb-4 backdrop-blur-sm bg-zinc-900 rounded-xl p-4"
                 href={`/packages/${post.slug}`}
               >
-                <div className="w-full flex flex-col">
-                  <p className="tracking-tight text-secondary dark:text-primary">
-                    {post.metadata.title}
-                  </p>
+                <div className="w-full flex flex-col gap-2">
+                  <div className="flex items-center gap-2 text-white">
+                    <div className="flex items-center justify-center bg-red-500 rounded-lg p-2">
+                      <NpmIcon
+                        color="#fff"
+                        className="w-6 h-6 fill-white text-white"
+                      />
+                    </div>
+                    <div className="flex flex-row gap-2 items-center justify-center">
+                      <p className="text-lg font-medium">
+                        {post.metadata.title}
+                      </p>
+                      <p className="text-xs text-muted-foreground">
+                        {post.metadata.version}
+                      </p>
+                    </div>
+                  </div>
                   <p className="h-6 text-xs text-muted-foreground">
-                    {post.metadata.publishedAt}
+                    {post.metadata.summary}
                   </p>
                 </div>
               </Link>
