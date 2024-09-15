296f32ef923e7b0578b4a2519a73a2c308d50e33
GweepCreative <gweepcreative@gmail.com>
2024-09-15 16:55:20 +0300

    og images

---
+++ src/app/packages/[slug]/opengraph-image.js
@@ -0,0 +1,69 @@
+import { ImageResponse } from "next/og";
+import { OpenGraphImage } from "@/components/og-image";
+import { getRegularFont, getBoldFont } from "@/lib/fonts";
+import {
+  sharedTitle,
+  sharedDescription,
+  sharedImage,
+} from "@/app/shared-metadata";
+import { headers } from 'next/headers'
+export const runtime = "edge";
+export const alt = `${sharedTitle} - Packages`;
+export const size = {
+  width: sharedImage.width,
+  height: sharedImage.height,
+};
+export const contentType = sharedImage.type;
+
+/* export const getImage = async () => {
+  const response = await fetch(new URL('@/assets/me.jpg', import.meta.url))
+  const font = await response.arrayBuffer()
+  return font
+} */
+
+export default async function Image() {
+  const headersList = headers()
+const pathname = headersList.get('x-invoke-path') || ''
+  const [regularFontData, boldFontData] = await Promise.all([
+    getRegularFont(),
+    getBoldFont(),
+  ]);
+  
+ 
+  return new ImageResponse(
+    (
+      <OpenGraphImage
+        title={`${sharedTitle} - ${pathname.split("/")[2]}`}
+        description={"GrknDev's libraries published in npm"}
+        icon={
+          <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="96"
+          height="96"
+          viewBox="0 0 512 512"
+        >
+          <path d="M227.6 213.1H256v57.1h-28.4z" />
+          <path d="M0 156v171.4h142.2V356H256v-28.6h256V156zm142.2 142.9h-28.4v-85.7H85.3v85.7H28.4V184.6h113.8zm142.2 0h-56.9v28.6h-56.9V184.6h113.8zm199.2 0h-28.4v-85.7h-28.4v85.7h-28.4v-85.7H370v85.7h-56.9V184.6h170.7v114.3z" />
+        </svg>
+        }
+      />
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "SF Pro",
+          data: regularFontData,
+          style: "normal",
+          weight: 500,
+        },
+        {
+          name: "SF Pro",
+          data: boldFontData,
+          style: "normal",
+          weight: 600,
+        },
+      ],
+    }
+  );
+}
